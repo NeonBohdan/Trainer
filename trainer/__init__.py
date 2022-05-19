172371ac8c4a2dbfca80debd3629eb63604c1dba
NeonBohdan <bohdan@neon.ai>
2022-05-19 15:27:12 +0300

    Removed version file

--- trainer/__init__.py
+++ trainer/__init__.py
@@ -3,7 +3,5 @@ import os
 from trainer.model import *
 from trainer.trainer import *
 
-with open(os.path.join(os.path.dirname(__file__), "VERSION"), "r", encoding="utf-8") as f:
-    version = f.read().strip()
 
-__version__ = version
+__version__ = 'v0.0.11'
